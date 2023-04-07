fa9d8e8579e8faf377359aefd5464d8787b9ffcb
Jeremy Felt <jeremy.felt@gmail.com>
2023-04-07 15:43:05 -0700

    Add built files, not started files

--- js/build/meta-box.asset.php
+++ js/build/meta-box.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-core-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'f590001547ea63ed3a63');
+<?php return array('dependencies' => array('wp-components', 'wp-core-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '0e38c47bbfcdfa1b9c43');
